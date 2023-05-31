d3f39f5fa8d56f2d1c15bed895029061156f0c16
Gabriel Ferrandin <93287315+gabferr@users.noreply.github.com>
2023-05-31 14:07:52 -0300

    add scene
    prototype of gameplay

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Rigidbody2D myRigibody;
+
+    public float speed;
+
+    public Vector2 velocity;
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            //myRigibody.MovePosition(myRigibody.position - velocity * Time.deltaTime);
+            myRigibody.velocity = new Vector2(-speed, myRigibody.velocity.y);
+        }
+
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            //myRigibody.MovePosition(myRigibody.position + velocity * Time.deltaTime);
+            myRigibody.velocity = new Vector2(speed, myRigibody.velocity.y);
+        }
+    }
+}
